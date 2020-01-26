c93ba1365e809d400d0afb0da6fe21c167757d99
Tanvir  Ahmed <Ta>
2020-01-26 15:46:55 +0600

    model done

---
+++ ToDoListBook/ChecklisItem.swift
@@ -0,0 +1,14 @@
+//
+//  ChecklisItem.swift
+//  ToDoListBook
+//
+//  Created by Mac on 1/26/20.
+//  Copyright © 2020 JaKhushiTai. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+}
